ad438c72d0d238ac675d73b923cbc21e8c08e27c
Ronando Long <ronando.long@gmail.com>
2015-09-18 10:01:49 -0700

    It will take a multi-word string and replace one targeted word with another, using two arguments.

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,5 +1,11 @@
 class String
-  define_method(:find_and_replace) do
-    self
+  define_method(:find_and_replace) do |target, replacement|
+    string = self
+
+    if string.include?(target)
+      string.gsub target, replacement
+    else
+      string
+    end
   end
 end
--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -2,7 +2,15 @@ require('rspec')
 require('find_and_replace')
 
 describe("String#find_and_replace") do
-  it('will accept a string') do
-    expect(("Lord of the Rings").find_and_replace()).to(eq("Lord of the Rings"))
+  it('will return original string if target word is not found') do
+    expect(("Lord of the Rings").find_and_replace("boy", "girl")).to(eq("Lord of the Rings"))
+  end
+
+  it('will take a one word string and replace the word with another word') do
+    expect(("Batman").find_and_replace("Batman", "Superman")).to(eq("Superman"))
+  end
+
+  it('will take a multi word string and replace one word with another word') do
+    expect(("Lord of the Rings").find_and_replace("Rings", "Flies")).to(eq("Lord of the Flies"))
   end
 end
