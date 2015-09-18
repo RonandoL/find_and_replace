b50cbc1536dbebb973c86d3d930ac954211a0fa4
Ronando Long <ronando.long@gmail.com>
2015-09-18 10:04:43 -0700

    It will not replace a mixed case targeted word.

--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -13,4 +13,8 @@ describe("String#find_and_replace") do
   it('will take a multi word string and replace one word with another word') do
     expect(("Lord of the Rings").find_and_replace("Rings", "Flies")).to(eq("Lord of the Flies"))
   end
+
+  it('will take a multi word string and replace one word with another word') do
+    expect(("The boy she met is now her Boyfriend").find_and_replace("boy", "girl")).to(eq("The boy she met is now her Girlfriend"))
+  end
 end
